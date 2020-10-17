db50b290357315a63da7c4212b9a5882fe88bb6b
DGalexander <dga.1984@gmail.com>
2020-10-17 17:21:17 +0100

    Update of script
    Now includes subset required information

--- Covid-parse.py
+++ Covid-parse.py
@@ -15,6 +15,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# BS setup
 url = 'https://www.gov.uk/guidance/full-list-of-local-covid-alert-levels-by-area'
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
@@ -24,20 +25,7 @@ soup = BeautifulSoup(html, "html.parser")
 # this is not needed but it helps reduce the chance of
 # the sting being repeated elsewhere in the html
 div = soup.find("div", {"class": "govspeak"})
-#div2 = soup("h2")
 
-# this finds a specific string
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
-import re
-for tag in div.find_all(string=re.compile("Local COVID alert level: high")):
-    print(tag)
-
-
-# this finds all the next h3 but doesn't stop at Local COVID alert level: very high"
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all-next-and-find-next
-test= tag.find_all_next("h3")
-print(test)
-
-
-
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-previous-siblings-and-find-previous-sibling
+# subset by finding the tag 'name'
+for name in div.find_all(["h2", "h3"]):
+    print(name.contents[0])
